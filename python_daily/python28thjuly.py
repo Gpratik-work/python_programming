a8a5e65cf76c009108383fd003880c57eeb8a698
Pratik <pratikstudy2024@gmail.com>
2025-07-28 02:40:16 +0530

    28th july code

---
+++ python_daily/python28thjuly.py
@@ -0,0 +1,42 @@
+# Day 1:
+
+# Print "Hello, World!"
+# print("Hello world")
+# Add two numbers
+# a=int(input("Enter your first number:\n"))
+# b=int(input("Enter your second number:\n"))
+# print(a+b)
+
+# Swap two variables
+# a=int(input("Enter your first number:\n"))
+# b=int(input("Enter your second number:\n"))
+# a,b=b,a
+# print(a)
+# print(b)
+
+# with third variable
+# a=int(input("enter your first number:\n"))
+# b=int(input("Enter your second number:\n"))
+# temp=a
+# a=b
+# b=temp
+# print(a)
+# print(b)
+
+# Day 2:
+
+# Find the square root of a number
+# num=int(input("Enter your number:\n"))
+# sq=num**2
+# print(sq)
+
+# Calculate the area of a triangle
+# base=float(input("Enter your length:\n"))
+# height=float(input("Enter your height:\n"))
+# area_of_triangle=1/2*base*height
+# print(area_of_triangle)
+
+# Convert kilometers to miles
+# kilo=float(input("Enter your kilometer:\n"))
+# miles=kilo*0.6213
+# print(f"{kilo} kilometers ---> {miles} miles.")
