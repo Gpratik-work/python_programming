671852bce9d9c882ba2d55fd485a84d3b646b244
Pratik <pratikstudy2024@gmail.com>
2025-07-29 22:13:00 +0530

    day3 python programing

---
+++ python_daily/python29thjuly.py
@@ -0,0 +1,14 @@
+# Day 3:
+
+# Check if a number is even or odd
+# num=int(input("Enter your number:\n"))
+# if num %2==0:
+#     print("number is even")
+# else:
+#     print("number is odd")
+# Find the largest of three numbers
+a=[100,200,300,120,320,321]
+a.sort()
+print(a[-3:])
+
+# Simple calculator (using if-else)
